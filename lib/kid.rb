350ca05b0a6ae51f8b0ab6c7de6c69e334a9eb6a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-18 17:24:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,8 @@ require_relative './fancy_dance.rb'
 class Kid 
     include Dance
     extend MetaDancing
+    FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   attr_accessor :name
  
   def initialize(name)
